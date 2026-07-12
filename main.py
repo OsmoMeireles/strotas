0c1245f902bd19bc6132a705e0895a9b9fb3dfed
OsmoMeireles <142730491+OsmoMeireles@users.noreply.github.com>
2026-07-12 16:08:09 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as ps
-import streamlit as st
-import plotly.express as px
-
-st.set_page_config(page_title="Dados Meli",
-                   page_icon=":bar_chart:", layout="wide")
-
-st.title("Dashboard")
-st.image("meli.png", width=200)
