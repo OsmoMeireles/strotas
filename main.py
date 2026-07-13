4e19215812f9733ae7998d0e5e49ccb9c11a7eb1
OsmoMeireles <142730491+OsmoMeireles@users.noreply.github.com>
2026-07-13 20:29:35 -0300

    Integrate Streamlit for dashboard and filters
    Added Streamlit dashboard setup and transportadora selection.

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-[
-  "ADO",
-  "ALC",
-  "BASEPEX",
-  "BLD",
-  "CAXIENSE",
-  "COOPMETRO",
-  "DECARGO",
-  "DHL",
-  "HAWK",
-  "HELP",
-  "IMEDIATO",
-  "KANGU",
-  "NEPOMUCENO",
-  "P. SPOT",
-  "RODACOOP"
-]
+import pandas as ps
+import streamlit as st
+
+st.set_page_config(page_title="Dados Meli",
+                   page_icon=":bar_chart:", layout="wide")
+
+st.sidebar.title("Dashboard")
+
+st.sidebar.image("meli.png", width=100)
+st.sidebar.title("Filtros")
+transportadora = st.sidebar.selectbox("Selecione a transportadora:", [
+    "ADO",
+    "ALC",
+    "BASEPEX",
+    "BLD",
+    "CAXIENSE",
+    "COOPMETRO",
+    "DECARGO",
+    "DHL",
+    "HAWK",
+    "HELP",
+    "IMEDIATO",
+    "KANGU",
+    "NEPOMUCENO",
+    "P. SPOT",
+    "RODACOOP"
+])
